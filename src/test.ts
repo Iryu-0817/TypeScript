c489bca29f7550f92edc0ec709e1bc5f10787a61
Iryu-0817 <iryu20030817@gmail.com>
2024-09-02 11:51:15 +0900

    ・データ型の種類 ・ファイルの追加

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+export const test = () => {
+  const name: string = "John Doe";
+
+  const age: number = 30;
+  const isAdult: boolean = false;
+  const hobbies: string[] = ["reading", "coding", "gaming"];
+  const address: { street: string; city: string } = {
+    street: "123 Main St",
+    city: "New York",
+  };
+  const today: Date = new Date();
+  const none: null = null;
+  const notDefined: undefined = undefined;
+  const radom: any = "Hello World";
+};
